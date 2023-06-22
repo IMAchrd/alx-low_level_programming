a9fb0b29a756257176051a0571d9718846850aa7
charradi imane <charradiimane@gmail.com>
2023-06-22 09:30:52 -0700

    5th commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times nombers
+ *
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j / 10 != 0)
+				_putchar(j / 10 + '0');
+			else
+				_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
