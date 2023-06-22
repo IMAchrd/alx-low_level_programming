9e7df54c061e5d8a5e60ba25d2f33630296fd66a
charradi imane <charradiimane@gmail.com>
2023-06-22 08:42:15 -0700

    3rd commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - multiplies tow integers
+ *
+ * @a: Variable
+ *
+ * @b: variable
+ *
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
